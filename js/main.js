c1b171d55d666dcc93218c15dcee2f2ef142a27d
Damian <Damian@DESKTOP-E4AAR0H>
2020-07-16 17:37:55 -0600

    JS

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
-let contador = 0;
-
-boton.addEventListener("click", funcion());
-if(contador == 0){
-    enlaces.className = ('enlaces dos');
-    contador=1;
-}else{
-    enlaces.classList.remove('dos');
-    enlaces.className() = ('enlaces uno');
-    contador = 0;
-}
+// let boton = document.getElementById("icono");
+// let enlaces = document.getElementById("enlaces");
+// let contador = 0;
+//
+// boton.addEventListener("click", funcion());
+// if(contador == 0){
+//     enlaces.className = ('enlaces dos');
+//     contador=1;
+// }else{
+//     enlaces.classList.remove('dos');
+//     enlaces.className() = ('enlaces uno');
+//     contador = 0;
+// }
 
 
 var objDiv = document.getElementById("nav__item");
 objDiv.scrollTop = objDiv.scrollHeight;
+
+function cambiarfondo() {
+        var fondo = document.getElementById("color").value;
+        var body = document.getElementById("body");
+        body.style.backgroundColor = fondo;
+    }
